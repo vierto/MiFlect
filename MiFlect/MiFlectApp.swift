678adc54b2c6ddbb3ebf6b9a094ca00de63da8cd
Vendly <vendlytan@gmail.com>
2022-07-25 18:58:12 +0700

    add file to project

---
+++ MiFlect/MiFlectApp.swift
@@ -0,0 +1,20 @@
+//
+//  MiFlectApp.swift
+//  MiFlect
+//
+//  Created by Vendly on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MiFlectApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
